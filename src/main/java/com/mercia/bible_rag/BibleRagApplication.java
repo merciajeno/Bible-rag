54bd6bc6891081533833e8b6bdd4d1013270d210
Mercia Jeno <mercia.jeno@gmail.com>
2026-03-16 15:18:55 +0530

    maven start code

---
+++ src/main/java/com/mercia/bible_rag/BibleRagApplication.java
@@ -0,0 +1,13 @@
+package com.mercia.bible_rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleRagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercia/bible_rag/BibleRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercia.bible_rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
